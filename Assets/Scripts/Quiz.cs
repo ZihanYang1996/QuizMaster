1e4f6b2ea9508e818edda277d4e3e658c10c581c
Zihan Yang <ws704603301@gmail.com>
2024-02-22 16:31:50 -0500

    Add getter functions

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] QuestionSO question;
+    
+    void Start()
+    {
+        
+    }
+
+
+}
